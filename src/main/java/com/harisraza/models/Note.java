caa98ccffaa048f1cd8491651e1cc2739b364690
Muhammad Haris <wannaheadshot5@gmail.com>
2023-06-18 02:19:46 +0500

    init

---
+++ src/main/java/com/harisraza/models/Note.java
@@ -0,0 +1,21 @@
+package com.harisraza.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+}
